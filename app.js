8625bbfa5d79ca3b0120fc73e6bd868f0b5d9696
Sudhanshu Tiwari <sudhanshu21.st@gmail.com>
2019-07-17 01:55:06 +0530

    Port changed to 5002

--- app.js
+++ app.js
@@ -61,5 +61,5 @@ function messageReceived(data){
 	}	
 }
 
-httpServer.listen(5001, httpServerConnected);
+httpServer.listen(5002, httpServerConnected);
 ioServer.on('connection', ioServerConnected);
